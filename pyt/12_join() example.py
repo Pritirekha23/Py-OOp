0b512fa9b37417bad62f577916665c260944d20d
Pritirekha23 <learnpriti23@gmail.com>
2023-05-09 10:00:39 +0530

    DAy 71 of 100Days of code

--- pyt/12_join() example.py
+++ pyt/12_join() example.py
@@ -1,6 +1,8 @@
 from threading import*
 import time
 
+
+print('example----2')
 def fun1():
     for i in range(3):
         print('fun1')
@@ -34,3 +36,56 @@ fun3
 fun2
 fun3
 fun3  '''
+
+
+print('example--3')
+
+def f1():
+    for i in range(3):
+        time.sleep(3)
+        print('Hi i am priti')
+        
+t1=Thread(target=f1)
+t1.start()
+
+t1.join(5)  #main thread will wair 5 sec 
+for i in range(3):
+    print('How are you?')
+    
+    
+    '''
+    op:
+Hi i am priti
+How are you?
+How are you?
+How are you?
+Hi i am priti
+Hi i am priti  '''
+
+
+
+print('example--4')
+
+def f1():
+    for i in range(3):
+        time.sleep(3) #t1 thread will wait 3 sec
+        print('Hi i am priti')
+        
+t1=Thread(target=f1)
+t1.start()
+
+t1.join(5)  #main thread will wait 5 sec 
+for i in range(3):
+    time.sleep(2)  #main thread  will wait 2 sec
+    print('How are you?')
+    
+    
+#     op:
+# Hi i am priti
+# Hi i am priti
+# Hi i am priti
+# Hi i am priti
+# How are you?
+# How are you?
+# Hi i am priti
+# How are you?
